476e948436a976d6c49176c46e7c2aad1a3e288e
ganitkumar517 <jeetmehra517@gmail.com>
2025-03-10 15:14:51 +0530

    init: tailwindcss integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className="container">
-      <h1 className="text-2xl font-bold">Live Cab Tracker</h1>
+      <h1 className="text-4xl font-bold">Live Cab Tracker</h1>
       <CabTracker />
     </div>
   );
